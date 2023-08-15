0d0eeeccf62d14f1ec85ae8dbd16099ecb2e396a
Anish Neupane <anishnep51@gmail.com>
2023-08-16 00:49:31 +0545

    Project 2 Day 2

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="180x180" href="../icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../icons/favicon-16x16.png">
+    <link rel="manifest" href="../icons/site.webmanifest">
+    <title>Lab Report Management System</title>
+    <link rel="stylesheet" href="../css/index.css">
+</head>
+
+<body>
+    <?php include "./php/navbar.php" ?>
+    <main>
+        <div class="contents">
+            <div id="top">Our Services</div>
+            <div class="content" id="content1">
+                <h1>Pathology Tests</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex at reiciendis quasi, consequatur sequi tempore officia esse iure nulla, explicabo dolor quibusdam voluptate nemo laudantium, maxime molestias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque accusantium in aliquid, ipsam animi ducimus minus eum odit ad nobis veritatis fuga velit, sunt amet repudiandae! Consequuntur enim rerum dolor! .Est, assumenda cupiditate.</p>
+            </div>
+            <div id="content2" class="content">
+                <h1>Pathology Tests</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex at reiciendis quasi, consequatur sequi tempore officia esse iure nulla, explicabo dolor quibusdam voluptate nemo laudantium, maxime molestias. Est, assumenda cupiditate.</p>
+            </div>
+            <div class="content" id="content3">
+                <h1>Pathology Tests</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex at reiciendis quasi, consequatur sequi tempore officia esse iure nulla, explicabo dolor quibusdam voluptate nemo laudantium, maxime molestias. Est, assumenda cupiditate.</p>
+            </div>
+            <div class="content" id="content4">
+                <h1>Lab Report Everywhere</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aperiam molestiae ducimus ipsum quo, qui nihil. Eaque ullam voluptatem quidem beatae atque blanditiis quos quas magni, quia esse minus ipsam.</p>
+            </div>
+            <div class="content" id="content5">
+                <h1>Easy Language and Interface</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat vel repellat at, doloribus, sint fuga ducimus aut eveniet fugit sunt necessitatibus molestiae quas. Velit accusantium sunt facilis molestias expedita praesentium.</p>
+            </div>
+        </div>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
