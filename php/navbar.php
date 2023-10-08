fef5794fd20dd0e50d7c7a1c831295a27119a9d3
Anish Neupane <anishnep51@gmail.com>
2023-10-08 21:03:40 +0545

    Logo button links to homepage, admin panel added

--- php/navbar.php
+++ php/navbar.php
@@ -3,8 +3,8 @@
 </head>
 
 <nav>
-    <div class="heading"><img style="height: 3em;" src="../images/website-logo.png"></div>
-    <div class="heading">Lab Report Management System</div>
+    <div class="heading" onclick="window.location.href = '../index.php'"><img style="height: 3em;" src="../images/website-logo.png"></div>
+    <div class="heading" onclick="window.location.href = '../index.php'">Lab Report Management System</div>
     <ul class="nav-items">
         <li class="nav-item" id="nav-item-1"><a href="../index.php" class="nav-anchor">Home</a></li>
         <li class="nav-item" id="nav-item-2"><a href="../php/newreport.php" class="nav-anchor">New Report</a></li>
@@ -16,6 +16,7 @@
     if(!isset($_SESSION['username'])){
         echo("<a id='button' href='../php/login.php'>Login</a>");
     }else{
-        echo("<a id='button' href='../php/logout.php'>Logout</a>");
+        echo("<a id='button' href='../php/admin.php'>Admin</a>
+        <a id='button' href='../php/logout.php'>Logout</a>");
     }?>
 </nav>
